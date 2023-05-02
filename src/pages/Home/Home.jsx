1e2b7087bfe46e04cc8c82b6af464fd13cf0b697
Tanvir Ahmad <tanvirahmadstudent@gmail.com>
2023-05-02 19:04:34 +0600

    implement banner section in home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,42 @@ import slide3 from '../../assets/slide3.png'
 const Home = () => {
     return (
         <div className='mx-4 md:mx-12'>
-         
+            <div className='flex flex-col md:flex-row justify-between items-center'>
+                <div className=' p-5 '>
+                    <h1 className='text-5xl font-medium'> Easy Cooking & Tasty Eating</h1>
+                    <p className='mt-2'>Discover, Cook, and Enjoy the Best Recipes from Around the World - Your Ultimate Recipe Guide</p>
+                </div>
+                <div className="carousel w-full ">
+                    <div id="slide1" className="carousel-item relative w-full">
+                        <img src={slide1} className="w-full rounded-xl" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide3" className="btn btn-circle">❮</a>
+                            <a href="#slide2" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide2" className="carousel-item relative w-full">
+                        <img src={slide2} className="w-full rounded-xl" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide1" className="btn btn-circle">❮</a>
+                            <a href="#slide3" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide3" className="carousel-item relative w-full">
+                        <img src={slide3} className="w-full rounded-xl" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide2" className="btn btn-circle">❮</a>
+                            <a href="#slide1" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide4" className="carousel-item relative w-full">
+                        <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide3" className="btn btn-circle">❮</a>
+                            <a href="#slide1" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
